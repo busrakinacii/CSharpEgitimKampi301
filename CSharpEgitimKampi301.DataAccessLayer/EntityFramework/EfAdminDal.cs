2915d9ad933c771cff046e2c930edfa949b63205
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-12-08 23:15:54 +0300

    EnttiyFramework Classes Added and GenericRepository Entity State Added

---
+++ CSharpEgitimKampi301.DataAccessLayer/EntityFramework/EfAdminDal.cs
@@ -0,0 +1,22 @@
+﻿using CSharpEgitimKampi301.DataAccessLayer.Abstract;
+using CSharpEgitimKampi301.DataAccessLayer.Repositories;
+using CSharpEgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.EntityFramework
+{
+    public class EfAdminDal : GenericRepository<Admin>, IAdminDal
+    {
+
+    }
+}
+/*
+ * Ekle,Sil,Güncelle,Listele ve Id'ye göre Getir
+ * Entityye özgü olmayan metotlar
+ * sistemdeki bütün entityler kullanabilir
+ * İçinde a harfi geçmeyen kullanıcıları liseleme bu sadece bir entitye özgü olur
+ */
---
+++ CSharpEgitimKampi301.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,55 @@
+﻿using CSharpEgitimKampi301.DataAccessLayer.Abstract;
+using CSharpEgitimKampi301.DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        CampContext contex = new CampContext();
+        private readonly DbSet<T> _object;
+
+        //ctor tab tab
+        //Entity State Kullnaımı
+        public GenericRepository()
+        {
+            _object = contex.Set<T>();
+        }
+        public void Delete(T entity)
+        {
+            var deletedEntity = contex.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
+            contex.SaveChanges();
+
+        }
+
+        public List<T> GetAll()
+        {
+            return _object.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _object.Find(id);
+        }
+
+        public void insert(T entity)
+        {
+            var addedEntity = contex.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            contex.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            var updatedEntity = contex.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
+            contex.SaveChanges();
+        }
+    }
+}
